3341fe2e0175fc9332171f35e90d7b63c2ae810b
Chinmay Patel <chinmay@chinmay.ca>
2020-06-27 17:43:04 -0400

    Adding .env configuration & datbase connection

--- app/server.js
+++ app/server.js
@@ -1,4 +1,17 @@
 var express = require('express');
+require('dotenv').config();
+
+const { Sequelize } = require('sequelize');
+const sequelize = new Sequelize(process.env.connectionString);
+
+sequelize.authenticate().then(function () {
+  console.log('Connection has been established successfully.');
+})
+  .catch(function (error) {
+    console.error('Unable to connect to the database:', error);
+  });
+
+
 var graphqlHTTP = require('express-graphql');
 var { buildSchema } = require('graphql');
 
@@ -27,4 +40,4 @@ app.use('/graphql', graphqlHTTP({
 }));
 
 app.listen(4000);
-console.log('Running a graphql server at http://localhost:4000/graphql');
+console.log(process.env.applicationName + ' is running a graphql server at http://localhost:4000/graphql');
